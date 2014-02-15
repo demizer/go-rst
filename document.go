094778627bdbae22f58f785c785a8e9619e3fc44
Jesus Alvarez <jeezusjr@gmail.com>
2014-02-15 03:35:08 -0800

    document.go: Add Parse()

--- document.go
+++ document.go
@@ -5,6 +5,7 @@ package rst
 
 import (
 	"github.com/demizer/go-rst/parse"
+	"fmt"
 )
 
 type Document struct {
@@ -13,8 +14,16 @@ type Document struct {
 }
 
 func New(name string) *Document {
-
 	return &Document{
 		name: name,
 	}
+}
+
+func (d *Document) Parse(text string) (*Document, error) {
+	tree, err := parse.Parse(d.name, text)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("%#v\n", tree)
+	return nil, nil
 }
