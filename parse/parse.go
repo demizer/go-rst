1e2c7ea3993a8524c2ee04b9fcb1575aeec2ac20
Jesus Alvarez <jeezusjr@gmail.com>
2014-02-15 03:30:08 -0800

    parse.go: Add basic functions
    Functions copied from text/parse/parse.go from the standard library.

--- parse/parse.go
+++ parse/parse.go
@@ -4,10 +4,19 @@
 package parse
 
 type Tree struct {
-	Name string
-	text string
-	// Document *ListNode
-	lex *lexer
+	Name      string
+	text      string
+	Root      *ListNode
+	lex       *lexer
+	peekCount int
+	token     [3]item // three-token lookahead for parser.
+}
+
+func Parse(name, text string) (t *Tree, err error) {
+	t = New(name)
+	t.text = text
+	_, err = t.Parse(text, t)
+	return
 }
 
 func New(name string) *Tree {
@@ -16,6 +25,40 @@ func New(name string) *Tree {
 	}
 }
 
-func (t *Tree) Parse(text string) (tree *Tree, err error) {
-	return nil, nil
+// startParse initializes the parser, using the lexer.
+func (t *Tree) startParse(lex *lexer) {
+	t.Root = nil
+	t.lex = lex
+}
+
+// stopParse terminates parsing.
+func (t *Tree) stopParse() {
+	t.lex = nil
+}
+
+func (t *Tree) Parse(text string, treeSet *Tree) (tree *Tree, err error) {
+	t.startParse(lex(t.Name, text))
+	t.text = text
+	t.parse(treeSet)
+	return t, nil
+}
+
+func (t *Tree) parse(tree *Tree) (next Node) {
+	t.Root = newList(t.peek().pos)
+	for t.peek().typ != itemEOF {
+		//
+	}
+	return nil
+
+}
+
+// peek returns but does not consume the next token.
+func (t *Tree) peek() item {
+	if t.peekCount > 0 {
+		return t.token[t.peekCount-1]
+
+	}
+	t.peekCount = 1
+	t.token[0] = t.lex.nextItem()
+	return t.token[0]
 }
