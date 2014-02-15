e7f0e78ba01b7d348b2cc6cc0f7013f541715bfc
Jesus Alvarez <jeezusjr@gmail.com>
2014-02-15 03:26:21 -0800

    node.go: Add basic functions
    Copied from text/template.go from the standard library.

--- parse/node.go
+++ parse/node.go
@@ -3,4 +3,57 @@
 // MIT Licensed. See LICENSE for details.
 package parse
 
+import (
+	"bytes"
+	"fmt"
+)
+
+type Node interface {
+	Type() NodeType
+	String() string
+	Position() Pos
+}
+
+// Pos represents a byte position in the original input text from which
+// this template was parsed.
 type Pos int
+
+func (p Pos) Position() Pos {
+	return p
+}
+
+// NodeType identifies the type of a parse tree node.
+type NodeType int
+
+const (
+	NodeSection NodeType = iota
+	NodeParagraph
+	NodeList
+)
+
+// ListNode holds a sequence of nodes.
+type ListNode struct {
+	NodeType
+	Pos
+	Nodes []Node // The element nodes in lexical order.
+
+}
+
+func newList(pos Pos) *ListNode {
+	return &ListNode{NodeType: NodeList, Pos: pos}
+}
+
+func (l *ListNode) append(n Node) {
+	l.Nodes = append(l.Nodes, n)
+
+}
+
+func (l *ListNode) String() string {
+	b := new(bytes.Buffer)
+	for _, n := range l.Nodes {
+		fmt.Fprint(b, n)
+
+	}
+	return b.String()
+
+}
