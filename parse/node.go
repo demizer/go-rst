8a934b62f8a930e3bf5f3e240465050654eda26d
Jesus Alvarez <jeezusjr@gmail.com>
2014-05-10 02:55:14 -0700

    node.go: Rewrite
    Add Node types implementing the Node interface. Most notably SectionNode.

--- parse/node.go
+++ parse/node.go
@@ -4,52 +4,147 @@
 
 package parse
 
-import (
-	"bytes"
-	"fmt"
-)
-
-type Node interface {
-	Type() NodeType
-	String() string
-	Position() Pos
-}
-
 // NodeType identifies the type of a parse tree node.
 type NodeType int
 
 const (
 	NodeSection NodeType = iota
 	NodeParagraph
-	NodeList
+	NodeBlankLine
+	NodeAdornment
 )
 
+var nodeTypes = [...]string{
+	"NodeSection",
+	"NodeParagraph",
+	"NodeBlankLine",
+	"NodeAdornment",
+}
+
+func (n NodeType) Type() NodeType {
+	return n
+}
+
+func (n NodeType) String() string {
+	return nodeTypes[n]
+}
+
+func (n NodeType) MarshalText() ([]byte, error) {
+	return []byte(n.String()), nil
+}
+
+type Node interface {
+	LineNumber() Line
+	NodeType() NodeType
+	Position() StartPosition
+}
+
+type NodeList []Node
+
+func newList() *NodeList {
+	return new(NodeList)
+}
+
+func (l *NodeList) append(n Node) {
+	*l = append(*l, n)
 
 }
 
-// ListNode holds a sequence of nodes.
-type ListNode struct {
-	NodeType
-	Pos
-	Nodes []Node // The element nodes in lexical order.
-
+type SectionNode struct {
+	Text      string
+	Level     int
+	Length    int
+	OverLine  *AdornmentNode
+	UnderLine *AdornmentNode
+	Nodes     NodeList
+	StartPosition
+	Line
+	Type NodeType
 }
 
-func newList(pos Pos) *ListNode {
-	return &ListNode{NodeType: NodeList, Pos: pos}
+func (s *SectionNode) NodeType() NodeType {
+	return s.Type
 }
 
-func (l *ListNode) append(n Node) {
-	l.Nodes = append(l.Nodes, n)
-
-}
-
-func (l *ListNode) String() string {
-	b := new(bytes.Buffer)
-	for _, n := range l.Nodes {
-		fmt.Fprint(b, n)
-
+func newSection(item item, level int, overAdorn item, underAdorn item) *SectionNode {
+	n := &SectionNode{Text: item.Value.(string),
+		Type:          NodeSection,
+		Level:         level,
+		StartPosition: item.StartPosition,
+		Length:        item.Length,
 	}
-	return b.String()
 
+	if overAdorn.Value != nil {
+		oRune := rune(overAdorn.Value.(string)[0])
+		n.OverLine = &AdornmentNode{
+			Char:          oRune,
+			Type:          NodeAdornment,
+			StartPosition: overAdorn.StartPosition,
+			Line:          overAdorn.Line,
+			Length:        overAdorn.Length,
+		}
+	}
+
+	uRune := rune(underAdorn.Value.(string)[0])
+	n.UnderLine = &AdornmentNode{
+		Char:          uRune,
+		Type:          NodeAdornment,
+		StartPosition: underAdorn.StartPosition,
+		Line:          underAdorn.Line,
+		Length:        underAdorn.Length,
+	}
+
+	return n
+}
+
+type AdornmentNode struct {
+	Char          rune     `json:"char"`
+	Length        int      `json:"length"`
+	Type          NodeType `json:"node-type"`
+	Line          `json:"line"`
+	StartPosition `json:"position"`
+}
+
+func (a AdornmentNode) NodeType() NodeType {
+	return a.Type
+}
+
+func newBlankLine(i item) *BlankLineNode {
+	return &BlankLineNode{
+		Type:          NodeBlankLine,
+		StartPosition: i.StartPosition,
+		Line:          i.Line,
+	}
+}
+
+type BlankLineNode struct {
+	Type          NodeType `json:"node-type"`
+	Line          `json:"line"`
+	StartPosition `json:"position"`
+}
+
+func (b BlankLineNode) NodeType() NodeType {
+	return b.Type
+}
+
+type ParagraphNode struct {
+	Text          string   `json:"text"`
+	Length        int      `json:"length"`
+	Type          NodeType `json:"node-type"`
+	Line          `json:"line"`
+	StartPosition `json:"position"`
+}
+
+func newParagraph(i item) *ParagraphNode {
+	return &ParagraphNode{
+		Text:          i.Value.(string),
+		Type:          NodeParagraph,
+		StartPosition: i.StartPosition,
+		Line:          i.Line,
+		Length:        i.Length,
+	}
+}
+
+func (p ParagraphNode) NodeType() NodeType {
+	return p.Type
 }
