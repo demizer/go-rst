4001a511ddc500f167bac11ac733f068ae63cc17
Jesus Alvarez <jeezusjr@gmail.com>
2014-05-20 01:40:34 -0700

    parse_test.go: Reorder functions

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -13,85 +13,6 @@ import (
 	"testing"
 )
 
-func TestSectionLevelsAdd(t *testing.T) {
-	var p sectionLevels
-	lvl := p.Add('=', true, 5)
-	if lvl != 1 {
-		t.Errorf("Improper level on first add, Got level: %d, expected: %d", lvl, 1)
-	}
-}
-
-func TestSectionLevelsString(t *testing.T) {
-	var p sectionLevels
-	p.Add('=', true, 5)
-	p.Add('-', true, 8)
-	p.Add('~', false, 6)
-	out := p.String()
-	expect := "level: 1, rune: '=', overline: true, length: 5\nlevel: 2, rune: '-', " +
-		"overline: true, length: 8\nlevel: 3, rune: '~', overline: false, length: 6\n"
-	if out != expect {
-		t.Errorf("String output mismatch!\nExpect:\n\n\t%q,\nGot:\n\n\t%q\n", expect, out)
-	}
-}
-
-func TestSectionLevelsFind(t *testing.T) {
-	var p sectionLevels
-	p.Add('=', true, 5)
-	p.Add('-', true, 8)
-	p.Add('~', false, 6)
-	lvl := p.Find('-')
-	if lvl == -1 {
-		t.Errorf("Level not found!\nExpect:\n\n\t%d\nGot:\n\n\t%t\n", 2, lvl)
-	}
-	if lvl != 2 {
-		t.Errorf("Level not correct!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", 2, lvl)
-	}
-}
-
-func TestSectionLevelsFindNoResult(t *testing.T) {
-	var p sectionLevels
-	lvl := p.Find('-')
-	if lvl > 0 {
-		t.Errorf("Should not find any levels!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", -1, lvl)
-	}
-}
-
-func TestSectionLevelsLevelEmpty(t *testing.T) {
-	var p sectionLevels
-	lvl := p.Level()
-	expect := 0
-	if lvl != expect {
-		t.Errorf("Empty sectionLevels should return \"%d\"!\nExpect:\t%d\nGot:\t%d\n",
-			expect, expect, lvl)
-	}
-
-}
-
-func TestSectionLevelsLevel(t *testing.T) {
-	var p sectionLevels
-	p.Add('=', true, 5)
-	p.Add('-', true, 8)
-	p.Add('~', false, 6)
-	lvl := p.Level()
-	if lvl != 3 {
-		t.Errorf("Level() returned incorrect level!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", 3, lvl)
-	}
-}
-
-func parseTest(t *testing.T, lexTest *LexTest) (tree *Tree) {
-	var errs []error
-	log.Debugf("Test Name: %s\n", lexTest.name)
-	log.Debugf("Description: %s\n", lexTest.description)
-	log.Debugf("Test Input:\n-----------\n%s\n----------\n", lexTest.data)
-	tree, errs = Parse(lexTest.name, lexTest.data)
-	if errs != nil {
-		for _, err := range errs {
-			t.Error(err)
-		}
-	}
-	return
-}
-
 type checkNode struct {
 	t          *testing.T
 	testName   string
@@ -209,6 +130,85 @@ func checkParseNodes(t *testing.T, eTree []interface{}, pNodes []Node, testName 
 	}
 	for eNum, eNode := range eTree {
 		state.checkFields(eNode, pNodes[eNum])
+	}
+	return
+}
+
+func TestSectionLevelsAdd(t *testing.T) {
+	var p sectionLevels
+	lvl := p.Add('=', true, 5)
+	if lvl != 1 {
+		t.Errorf("Improper level on first add, Got level: %d, expected: %d", lvl, 1)
+	}
+}
+
+func TestSectionLevelsString(t *testing.T) {
+	var p sectionLevels
+	p.Add('=', true, 5)
+	p.Add('-', true, 8)
+	p.Add('~', false, 6)
+	out := p.String()
+	expect := "level: 1, rune: '=', overline: true, length: 5\nlevel: 2, rune: '-', " +
+		"overline: true, length: 8\nlevel: 3, rune: '~', overline: false, length: 6\n"
+	if out != expect {
+		t.Errorf("String output mismatch!\nExpect:\n\n\t%q,\nGot:\n\n\t%q\n", expect, out)
+	}
+}
+
+func TestSectionLevelsFind(t *testing.T) {
+	var p sectionLevels
+	p.Add('=', true, 5)
+	p.Add('-', true, 8)
+	p.Add('~', false, 6)
+	lvl := p.Find('-')
+	if lvl == -1 {
+		t.Errorf("Level not found!\nExpect:\n\n\t%d\nGot:\n\n\t%t\n", 2, lvl)
+	}
+	if lvl != 2 {
+		t.Errorf("Level not correct!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", 2, lvl)
+	}
+}
+
+func TestSectionLevelsFindNoResult(t *testing.T) {
+	var p sectionLevels
+	lvl := p.Find('-')
+	if lvl > 0 {
+		t.Errorf("Should not find any levels!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", -1, lvl)
+	}
+}
+
+func TestSectionLevelsLevelEmpty(t *testing.T) {
+	var p sectionLevels
+	lvl := p.Level()
+	expect := 0
+	if lvl != expect {
+		t.Errorf("Empty sectionLevels should return \"%d\"!\nExpect:\t%d\nGot:\t%d\n",
+			expect, expect, lvl)
+	}
+
+}
+
+func TestSectionLevelsLevel(t *testing.T) {
+	var p sectionLevels
+	p.Add('=', true, 5)
+	p.Add('-', true, 8)
+	p.Add('~', false, 6)
+	lvl := p.Level()
+	if lvl != 3 {
+		t.Errorf("Level() returned incorrect level!\nExpect:\n\n\t%d\nGot:\n\n\t%d\n", 3, lvl)
+	}
+}
+
+func parseTest(t *testing.T, lexTest *LexTest) (tree *Tree) {
+	var errs []error
+	log.Debugf("Test Name: %s\n", lexTest.name)
+	log.Debugf("Description: %s\n", lexTest.description)
+	log.Debugf("Test Input:\n-----------\n%s\n----------\n", lexTest.data)
+	tree, errs = Parse(lexTest.name, lexTest.data)
+	if errs != nil {
+		for _, err := range errs {
+			t.Error(err)
+		}
 	}
 	return
 }
