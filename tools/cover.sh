6fd7d92d8ae970ebaed8f8db791e314d5b099e79
Jesus Alvarez <jeezusjr@gmail.com>
2016-02-15 01:02:31 -0800

    drone.io coveralls upload

--- tools/cover.sh
+++ tools/cover.sh
@@ -34,7 +34,7 @@ show_cover_report() {
 
 push_to_coveralls() {
     echo "Pushing coverage statistics to coveralls.io"
-    goveralls -coverprofile="$profile"
+    goveralls -coverprofile="$profile" -service drone.io -repotoken $COVERALLS_TOKEN
 }
 
 generate_cover_data parse
