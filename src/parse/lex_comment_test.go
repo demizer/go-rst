a1c9bb9e1e7538ebed171e63ed8169631a2eb1f5
Jesus Alvarez <jeezusjr@gmail.com>
2016-02-29 12:15:16 -0800

    Serialize comment test names

--- src/parse/lex_comment_test.go
+++ src/parse/lex_comment_test.go
@@ -3,136 +3,136 @@ package parse
 import "testing"
 
 // A single comment
-func TestLexCommentGood0000(t *testing.T) {
-	testPath := testPathFromName("00.00-comment")
-	test := LoadLexTest(t, testPath)
-	items := lexTest(t, test)
-	equal(t, test.expectItems(), items)
-}
-
-// A single comment block split with a newline
-func TestLexCommentBlockGood0001(t *testing.T) {
-	testPath := testPathFromName("00.01-comment-block")
-	test := LoadLexTest(t, testPath)
-	items := lexTest(t, test)
-	equal(t, test.expectItems(), items)
-}
-
-// A comment block that begins on the second line after the comment mark
-func TestLexCommentBlockOnSecondLineGood0100(t *testing.T) {
-	testPath := testPathFromName("01.00-comment-block-second-line")
-	test := LoadLexTest(t, testPath)
-	items := lexTest(t, test)
-	equal(t, test.expectItems(), items)
-}
-
-// One comment after another
-func TestLexTwoCommentsGood0200(t *testing.T) {
-	testPath := testPathFromName("02.00-two-comments")
+func Test_00_00_00_00_LexCommentGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.00-comment-good-comment")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // One comment not followed by a blank line
-func TestLexCommentNoBlankLineBad0000(t *testing.T) {
-	testPath := testPathFromName("00.00-comment-no-blankline")
+func Test_00_00_00_01_LexCommentNoBlankLineBad(t *testing.T) {
+	testPath := testPathFromName("00.00.00.01-comment-bad-comment-no-blankline")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
-// Two comments, no blank line after second comment
-func TestLexTwoCommentsNoBlankLineBad0001(t *testing.T) {
-	testPath := testPathFromName("00.01-two-comments-no-blankline")
+// A single comment block split with a newline
+func Test_00_00_00_02_LexCommentBlockGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.02-comment-good-comment-block")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment ending with a literal block mark.
-func TestLexCommentWithLiteralMarkGood0002(t *testing.T) {
-	testPath := testPathFromName("00.02-comment-with-literal-mark")
+func Test_00_00_00_03_LexCommentWithLiteralMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.03-comment-good-comment-with-literal-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment block with a newline after the comment mark
-func TestLexNewlineAfterCommentMarkGood0003(t *testing.T) {
-	testPath := testPathFromName("00.03-newline-after-comment-mark")
+func Test_00_00_00_04_LexNewlineAfterCommentMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.04-comment-good-newline-after-comment-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment block with a newline after the comment mark
-func TestLexNewlineAfterCommentMarkGood0004(t *testing.T) {
-	testPath := testPathFromName("00.04-newline-after-comment-mark")
+func Test_00_00_00_05_LexNewlineAfterCommentMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.05-comment-good-newline-after-comment-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment block with citation syntax in the text
-func TestLexCommentNotCitationGood0005(t *testing.T) {
-	testPath := testPathFromName("00.05-comment-not-citation")
+func Test_00_00_00_06_LexCommentNotCitationGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.06-comment-good-comment-not-citation")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment block with substitution definition syntax in the text
-func TestLexCommentNotSubstitutionDefinitionGood0005(t *testing.T) {
-	testPath := testPathFromName("00.06-comment-not-subs-def")
+func Test_00_00_00_07_LexCommentNotSubstitutionDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.07-comment-good-comment-not-subs-def")
+	test := LoadLexTest(t, testPath)
+	items := lexTest(t, test)
+	equal(t, test.expectItems(), items)
+}
+
+// A comment block that begins on the second line after the comment mark
+func Test_00_00_01_00_LexCommentBlockOnSecondLineGood(t *testing.T) {
+	testPath := testPathFromName("00.00.01.00-comment-good-comment-block-second-line")
+	test := LoadLexTest(t, testPath)
+	items := lexTest(t, test)
+	equal(t, test.expectItems(), items)
+}
+
+// One comment after another
+func Test_00_00_02_00_LexTwoCommentsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.02.00-comment-good-two-comments")
+	test := LoadLexTest(t, testPath)
+	items := lexTest(t, test)
+	equal(t, test.expectItems(), items)
+}
+
+// Two comments, no blank line after second comment
+func Test_00_00_02_01_LexTwoCommentsNoBlankLineBad(t *testing.T) {
+	testPath := testPathFromName("00.00.02.01-comment-bad-two-comments-no-blankline")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // An empty comment followed by a blockquote
-func TestLexCommentWithBlockquoteGood0300(t *testing.T) {
-	testPath := testPathFromName("03.00-empty-comment-with-blockquote")
+func Test_00_00_03_00_LexCommentWithBlockquoteGood(t *testing.T) {
+	testPath := testPathFromName("00.00.03.00-comment-good-empty-comment-with-blockquote")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A definition list with a comment in the definition
-func TestLexCommentInDefinitionGood0400(t *testing.T) {
-	testPath := testPathFromName("04.00-comment-in-definition")
+func Test_00_00_04_00_LexCommentInDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.04.00-comment-good-comment-in-definition")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment after a definition
-func TestLexCommentAfterDefinitionGood0401(t *testing.T) {
-	testPath := testPathFromName("04.01-comment-after-definition")
+func Test_00_00_04_01_LexCommentAfterDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.04.01-comment-good-comment-after-definition")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment between two bullet paragraphs
-func TestLexCommentBetweenBulletParagrapsGood0500(t *testing.T) {
-	testPath := testPathFromName("05.00-comment-between-bullet-paragraphs")
+func Test_00_00_05_00_LexCommentBetweenBulletParagrapsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.00-comment-good-comment-between-bullet-paragraphs")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment between two ferns... I mean bullets.
-func TestLexCommentBetweenBulletsGood0501(t *testing.T) {
-	testPath := testPathFromName("05.01-comment-between-bullets")
+func Test_00_00_05_01_LexCommentBetweenBulletsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.01-comment-good-comment-between-bullets")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
 }
 
 // A comment trailing a bullet list item
-func TestLexCommentTrailingBulletGood0502(t *testing.T) {
-	testPath := testPathFromName("05.02-comment-trailing-bullet")
+func Test_00_00_05_02_LexCommentTrailingBulletGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.02-comment-good-comment-trailing-bullet")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.expectItems(), items)
--- src/parse/parse_comment_test.go
+++ src/parse/parse_comment_test.go
@@ -3,35 +3,8 @@ package parse
 import "testing"
 
 // A single comment
-func TestParseCommentGood0000(t *testing.T) {
-	testPath := testPathFromName("00.00-comment")
-	test := LoadParseTest(t, testPath)
-	pTree := parseTest(t, test)
-	eNodes := test.expectNodes()
-	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
-}
-
-// A single comment split with a newline
-func TestParseCommentBlockGood0001(t *testing.T) {
-	testPath := testPathFromName("00.01-comment-block")
-	test := LoadParseTest(t, testPath)
-	pTree := parseTest(t, test)
-	eNodes := test.expectNodes()
-	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
-}
-
-// A comment block that begins on the second line after the comment mark
-func TestParseCommentBlockOnSecondLineGood0100(t *testing.T) {
-	testPath := testPathFromName("01.00-comment-block-second-line")
-	test := LoadParseTest(t, testPath)
-	pTree := parseTest(t, test)
-	eNodes := test.expectNodes()
-	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
-}
-
-// One comment after another
-func TestParseTwoCommentsGood0200(t *testing.T) {
-	testPath := testPathFromName("02.00-two-comments")
+func Test_00_00_00_00_ParseCommentGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.00-comment-good-comment")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -39,17 +12,17 @@ func TestParseTwoCommentsGood0200(t *testing.T) {
 }
 
 // One comment not followed by a blank line
-func TestParseCommentNoBlankLineBad0000(t *testing.T) {
-	testPath := testPathFromName("00.00-comment-no-blankline")
+func Test_00_00_00_01_ParseCommentNoBlankLineBad(t *testing.T) {
+	testPath := testPathFromName("00.00.00.01-comment-bad-comment-no-blankline")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
 
-// Two comments, no blank line after second comment
-func TestParseTwoCommentsNoBlankLineBad0001(t *testing.T) {
-	testPath := testPathFromName("00.01-two-comments-no-blankline")
+// A single comment split with a newline
+func Test_00_00_00_02_ParseCommentBlockGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.02-comment-good-comment-block")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -57,8 +30,8 @@ func TestParseTwoCommentsNoBlankLineBad0001(t *testing.T) {
 }
 
 // A comment ending with a literal block mark.
-func TestParseCommentWithLiteralMarkGood0002(t *testing.T) {
-	testPath := testPathFromName("00.02-comment-with-literal-mark")
+func Test_00_00_00_03_ParseCommentWithLiteralMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.03-comment-good-comment-with-literal-mark")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -66,8 +39,8 @@ func TestParseCommentWithLiteralMarkGood0002(t *testing.T) {
 }
 
 // A comment block with a newline after the comment mark
-func TestParseNewlineAfterCommentMarkGood0003(t *testing.T) {
-	testPath := testPathFromName("00.03-newline-after-comment-mark")
+func Test_00_00_00_04_ParseNewlineAfterCommentMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.04-comment-good-newline-after-comment-mark")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -75,8 +48,8 @@ func TestParseNewlineAfterCommentMarkGood0003(t *testing.T) {
 }
 
 // A comment block with a newline after the comment mark
-func TestParseNewlineAfterCommentMarkGood0004(t *testing.T) {
-	testPath := testPathFromName("00.04-newline-after-comment-mark")
+func Test_00_00_00_05_ParseNewlineAfterCommentMarkGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.05-comment-good-newline-after-comment-mark")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -84,8 +57,8 @@ func TestParseNewlineAfterCommentMarkGood0004(t *testing.T) {
 }
 
 // A comment block with citation syntax in the text
-func TestParseCommentNotCitationGood0005(t *testing.T) {
-	testPath := testPathFromName("00.05-comment-not-citation")
+func Test_00_00_00_06_ParseCommentNotCitationGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.06-comment-good-comment-not-citation")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -93,8 +66,35 @@ func TestParseCommentNotCitationGood0005(t *testing.T) {
 }
 
 // A comment block with substitution definition syntax in the text
-func TestParseCommentNotSubstitutionDefinitionGood0006(t *testing.T) {
-	testPath := testPathFromName("00.06-comment-not-subs-def")
+func Test_00_00_00_07_ParseCommentNotSubstitutionDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.00.07-comment-good-comment-not-subs-def")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
+
+// A comment block that begins on the second line after the comment mark
+func Test_00_00_01_00_ParseCommentBlockOnSecondLineGood(t *testing.T) {
+	testPath := testPathFromName("00.00.01.00-comment-good-comment-block-second-line")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
+
+// One comment after another
+func Test_00_00_02_00_ParseTwoCommentsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.02.00-comment-good-two-comments")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
+
+// Two comments, no blank line after second comment
+func Test_00_00_02_01_ParseTwoCommentsNoBlankLineBad(t *testing.T) {
+	testPath := testPathFromName("00.00.02.01-comment-bad-two-comments-no-blankline")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -102,8 +102,8 @@ func TestParseCommentNotSubstitutionDefinitionGood0006(t *testing.T) {
 }
 
 // An empty comment followed by a blockquote
-func TestParseCommentWithBlockquoteGood0300(t *testing.T) {
-	testPath := testPathFromName("03.00-empty-comment-with-blockquote")
+func Test_00_00_03_00_ParseCommentWithBlockquoteGood(t *testing.T) {
+	testPath := testPathFromName("00.00.03.00-comment-good-empty-comment-with-blockquote")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -111,8 +111,8 @@ func TestParseCommentWithBlockquoteGood0300(t *testing.T) {
 }
 
 // A definition list with a comment in the definition
-func TestParseCommentInDefinitionGood0400(t *testing.T) {
-	testPath := testPathFromName("04.00-comment-in-definition")
+func Test_00_00_04_00_ParseCommentInDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.04.00-comment-good-comment-in-definition")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -120,8 +120,8 @@ func TestParseCommentInDefinitionGood0400(t *testing.T) {
 }
 
 // A comment after a definition
-func TestParseCommentAfterDefinitionGood0401(t *testing.T) {
-	testPath := testPathFromName("04.01-comment-after-definition")
+func Test_00_00_04_01_ParseCommentAfterDefinitionGood(t *testing.T) {
+	testPath := testPathFromName("00.00.04.01-comment-good-comment-after-definition")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -129,8 +129,8 @@ func TestParseCommentAfterDefinitionGood0401(t *testing.T) {
 }
 
 // A comment between two paragraphs in a bullet list item
-func TestParseCommentBetweenBulletParagraphsGood0500(t *testing.T) {
-	testPath := testPathFromName("05.00-comment-between-bullet-paragraphs")
+func Test_00_00_05_00_ParseCommentBetweenBulletParagraphsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.00-comment-good-comment-between-bullet-paragraphs")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -138,8 +138,8 @@ func TestParseCommentBetweenBulletParagraphsGood0500(t *testing.T) {
 }
 
 // A comment between two ferns... I mean bullets.
-func TestParseCommentBetweenBulletsGood0501(t *testing.T) {
-	testPath := testPathFromName("05.01-comment-between-bullets")
+func Test_00_00_05_01_ParseCommentBetweenBulletsGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.01-comment-good-comment-between-bullets")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
@@ -147,8 +147,8 @@ func TestParseCommentBetweenBulletsGood0501(t *testing.T) {
 }
 
 // A comment trailing a bullet list item
-func TestParseCommentTrailingBulletGood0502(t *testing.T) {
-	testPath := testPathFromName("05.02-comment-trailing-bullet")
+func Test_00_00_05_02_ParseCommentTrailingBulletGood(t *testing.T) {
+	testPath := testPathFromName("00.00.05.02-comment-good-comment-trailing-bullet")
 	test := LoadParseTest(t, testPath)
 	pTree := parseTest(t, test)
 	eNodes := test.expectNodes()
