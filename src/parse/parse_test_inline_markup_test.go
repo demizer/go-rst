e3542d183ce32ec9550239fd0f44f259d13c6347
Jesus Alvarez <jeezusjr@gmail.com>
2016-10-11 00:52:58 -0700

    Add test 02.02.00.00 Simple Emphasis Good

--- src/parse/parse_test_inline_markup_test.go
+++ src/parse/parse_test_inline_markup_test.go
@@ -34,3 +34,11 @@ func Test_02_01_00_00_ParseInlineMarkupStrongGood(t *testing.T) {
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
+
+func Test_02_02_00_00_ParseInlineMarkupSimpleEmphasisGood(t *testing.T) {
+	testPath := testPathFromName("02.02.00.00-emphasis-good-simple-emphasis")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
