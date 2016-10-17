c6bed07c0caabba3729ae3443567690ddb84af69
Jesus Alvarez <jeezusjr@gmail.com>
2016-10-16 23:58:18 -0700

    Add Test_02_02_03_00_ParseInlineMarkupEmphasisGood

--- src/parse/parse_test_inline_markup_test.go
+++ src/parse/parse_test_inline_markup_test.go
@@ -154,3 +154,11 @@ func Test_02_02_02_02_ParseInlineMarkupEmphasisGood(t *testing.T) {
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
+
+func Test_02_02_03_00_ParseInlineMarkupEmphasisGood(t *testing.T) {
+	testPath := testPathFromName("02.02.03.00-emphasis-surrounded-by-markup")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
