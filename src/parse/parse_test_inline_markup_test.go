7de16cd3e3a4a507782708b17e65718cf2b91a71
Jesus Alvarez <jeezusjr@gmail.com>
2016-10-16 23:26:14 -0700

    Add Test_02_02_02_01_ParseInlineMarkupEmphasisGood

--- src/parse/parse_test_inline_markup_test.go
+++ src/parse/parse_test_inline_markup_test.go
@@ -138,3 +138,11 @@ func Test_02_02_02_00_ParseInlineMarkupEmphasisGood(t *testing.T) {
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
+
+func Test_02_02_02_01_ParseInlineMarkupEmphasisGood(t *testing.T) {
+	testPath := testPathFromName("02.02.02.01-emphasis-with-asterisk")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
