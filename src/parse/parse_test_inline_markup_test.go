e565da5ccbd8630dcb7e1d9c34313d535baf9734
Jesus Alvarez <jeezusjr@gmail.com>
2016-10-20 00:21:24 -0700

    Add Test_03_03_01_00_ParseSectionTitleNumberedGood

--- src/parse/parse_test_inline_markup_test.go
+++ src/parse/parse_test_inline_markup_test.go
@@ -194,3 +194,11 @@ func Test_02_03_00_02_ParseInlineMarkupLiteralGood(t *testing.T) {
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
+
+func Test_02_03_01_00_ParseInlineMarkupLiteralGood(t *testing.T) {
+	testPath := testPathFromName("02.03.01.00-literal-with-backslash")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
