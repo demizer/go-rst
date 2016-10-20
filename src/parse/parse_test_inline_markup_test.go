069f909bec41ce95593d6c026cf55a10febe2acd
Jesus Alvarez <jeezusjr@gmail.com>
2016-10-20 00:25:33 -0700

    Add Test_02_03_01_01_ParseInlineMarkupLiteralGood

--- src/parse/parse_test_inline_markup_test.go
+++ src/parse/parse_test_inline_markup_test.go
@@ -202,3 +202,11 @@ func Test_02_03_01_00_ParseInlineMarkupLiteralGood(t *testing.T) {
 	eNodes := test.expectNodes()
 	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
 }
+
+func Test_02_03_01_01_ParseInlineMarkupLiteralGood(t *testing.T) {
+	testPath := testPathFromName("02.03.01.01-literal-with-middle-backslash")
+	test := LoadParseTest(t, testPath)
+	pTree := parseTest(t, test)
+	eNodes := test.expectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+}
