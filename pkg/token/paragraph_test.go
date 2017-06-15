83ff368863bd6a098c8ef30cc91ebe0a6380d3a7
Jesus Alvarez <jeezusjr@gmail.com>
2017-06-15 03:09:27 -0700

    Reorg paragraph tests

--- pkg/token/paragraph_test.go
+++ pkg/token/paragraph_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/demizer/go-rst/pkg/testutil"
 )
 
-// A simple one line paragraph
 func Test_02_00_00_00_LexParagraphGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("02.00.00.00-paragraph")
 	test := LoadLexTest(t, testPath)
@@ -14,32 +13,28 @@ func Test_02_00_00_00_LexParagraphGood(t *testing.T) {
 	equal(t, test.ExpectItems(), items)
 }
 
-// A simple two line paragraph
-func Test_02_00_00_01_LexParagraphWithLineBreakGood(t *testing.T) {
+func Test_02_00_00_01_LexParagraphGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("02.00.00.01-with-line-break")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A simple three line paragraph
-func Test_02_00_00_02_LexParagraphWithThreeLinesGood(t *testing.T) {
+func Test_02_00_00_02_LexParagraphGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("02.00.00.02-three-lines")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// Two paragraphs separated by a blank line
-func Test_02_00_01_00_LexTwoSeparateParagraphs(t *testing.T) {
+func Test_02_00_01_00_LexParagraphGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("02.00.01.00-two-paragraphs")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// Two three line paragraphs separated by a blank line
-func Test_02_00_01_01_LexTwoSeparateThreeLineParagraphs(t *testing.T) {
+func Test_02_00_01_01_LexParagraphGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("02.00.01.01-two-paragraphs-three-lines")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
