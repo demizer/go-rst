b7abaf7be9468ea06738c4670d530b8cc073c3c4
Jesus Alvarez <jeezusjr@gmail.com>
2017-06-15 02:30:41 -0700

    Regen comment_test.go

--- pkg/token/comment_test.go
+++ pkg/token/comment_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/demizer/go-rst/pkg/testutil"
 )
 
-// A single comment
 func Test_00_00_00_00_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.00.00-comment")
 	test := LoadLexTest(t, testPath)
@@ -14,136 +13,120 @@ func Test_00_00_00_00_LexCommentGood(t *testing.T) {
 	equal(t, test.ExpectItems(), items)
 }
 
-// One comment not followed by a blank line
-func Test_00_00_00_01_LexCommentNoBlankLineBad(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.01-comment-no-blankline")
+func Test_00_00_00_01_LexCommentBad(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.00.01-bad-comment-no-blankline")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A single comment block split with a newline
-func Test_00_00_00_02_LexCommentBlockGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.02-comment-block")
+func Test_00_00_00_02_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.00.02-comment-with-literal-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment ending with a literal block mark.
-func Test_00_00_00_03_LexCommentWithLiteralMarkGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.03-comment-with-literal-mark")
+func Test_00_00_00_03_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.00.03-comment-not-reference")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment block with a newline after the comment mark
-func Test_00_00_00_04_LexNewlineAfterCommentMarkGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.04-newline-after-comment-mark")
+func Test_00_00_01_00_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.01.00-comment-block")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment block with a newline after the comment mark
-func Test_00_00_00_05_LexNewlineAfterCommentMarkGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.05-newline-after-comment-mark")
+func Test_00_00_01_01_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.01.01-comment-block-second-line")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment block with citation syntax in the text
-func Test_00_00_00_06_LexCommentNotCitationGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.06-comment-not-citation")
+func Test_00_00_02_00_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.02.00-newline-after-comment-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment block with substitution definition syntax in the text
-func Test_00_00_00_07_LexCommentNotSubstitutionDefinitionGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.07-comment-not-subs-def")
+func Test_00_00_02_01_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.02.01-newline-after-comment-mark")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-func Test_00_00_00_08_LexCommentIsNotReferenceGood_NotImplemented(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.00.08-comment-not-reference")
+func Test_00_00_02_02_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.02.02-comment-not-citation")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment block that begins on the second line after the comment mark
-func Test_00_00_01_00_LexCommentBlockOnSecondLineGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.01.00-comment-block-second-line")
+func Test_00_00_02_03_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.02.03-comment-not-subs-def")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// One comment after another
-func Test_00_00_02_00_LexTwoCommentsGood(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.02.00-two-comments")
-	test := LoadLexTest(t, testPath)
-	items := lexTest(t, test)
-	equal(t, test.ExpectItems(), items)
-}
-
-// Two comments, no blank line after second comment
-func Test_00_00_02_01_LexTwoCommentsNoBlankLineBad(t *testing.T) {
-	testPath := testutil.TestPathFromName("00.00.02.01-two-comments-no-blankline")
-	test := LoadLexTest(t, testPath)
-	items := lexTest(t, test)
-	equal(t, test.ExpectItems(), items)
-}
-
-// An empty comment followed by a blockquote
-func Test_00_00_03_00_LexCommentWithBlockquoteGood(t *testing.T) {
+func Test_00_00_03_00_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.03.00-empty-comment-with-blockquote")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A definition list with a comment in the definition
-func Test_00_00_04_00_LexCommentInDefinitionGood(t *testing.T) {
+func Test_00_00_04_00_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.04.00-comment-in-definition")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment after a definition
-func Test_00_00_04_01_LexCommentAfterDefinitionGood(t *testing.T) {
+func Test_00_00_04_01_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.04.01-comment-after-definition")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment between two bullet paragraphs
-func Test_00_00_05_00_LexCommentBetweenBulletParagrapsGood(t *testing.T) {
+func Test_00_00_05_00_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.05.00-comment-between-bullet-paragraphs")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment between two ferns... I mean bullets.
-func Test_00_00_05_01_LexCommentBetweenBulletsGood(t *testing.T) {
+func Test_00_00_05_01_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.05.01-comment-between-bullets")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
 }
 
-// A comment trailing a bullet list item
-func Test_00_00_05_02_LexCommentTrailingBulletGood(t *testing.T) {
+func Test_00_00_05_02_LexCommentGood(t *testing.T) {
 	testPath := testutil.TestPathFromName("00.00.05.02-comment-trailing-bullet")
+	test := LoadLexTest(t, testPath)
+	items := lexTest(t, test)
+	equal(t, test.ExpectItems(), items)
+}
+
+func Test_00_00_06_00_LexCommentGood(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.06.00-two-comments")
+	test := LoadLexTest(t, testPath)
+	items := lexTest(t, test)
+	equal(t, test.ExpectItems(), items)
+}
+
+func Test_00_00_06_01_LexCommentBad(t *testing.T) {
+	testPath := testutil.TestPathFromName("00.00.06.01-bad-two-comments-no-blankline")
 	test := LoadLexTest(t, testPath)
 	items := lexTest(t, test)
 	equal(t, test.ExpectItems(), items)
