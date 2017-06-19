28fbe824225feee446ce20b3124ab7b5fdf80283
Jesus Alvarez <jeezusjr@gmail.com>
2017-06-18 18:25:27 -0700

    Use go generate to build parsing test suite
    Go tests for the lexer and parser can be generated with the go generate
    command.
    
    Generating the tests from the state of the testdata directory protects
    against human error when importing and implementing tests.
    
    In the last refactor, some errors in the Go test files were encountered
    where some tests were commented out and other tests were left out
    completely. These tests failed when activated correctly in the Go
    testing code.
    
    Additional benefits include: being able to remove seven "<element>_test.go" files
    and improved ability to reorganize tests.

--- parse.go
+++ parse.go
@@ -3,3 +3,5 @@
 // This package is only meant for lexing and parsing reStructuredText. See the top level package documentation for details on
 // using the go-rst package package API.
 package rst
+
+//go:generate go run tools/gentests.go
---
+++ tools/gentests.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var testFiles []testFile
+
+type testFile struct {
+	name        string
+	id          string
+	path        string
+	baseName    string
+	bad         bool
+	lexerTest   bool
+	parserTest  bool
+	implemented bool
+}
+
+type testPackage struct {
+	Name       string
+	OutputPath string
+	Tests      []test
+}
+
+type test struct {
+	Package      string
+	ID           string
+	Type         string
+	Name         string
+	FileBaseName string
+	Outcome      string
+	Implemented  bool
+}
+
+var testTemplate = `//
+// AUTO-GENERATED using tools/gentests.go
+//
+
+package {{.Name}}
+
+import (
+	"os"
+	"testing"
+
+	"github.com/demizer/go-rst/pkg/testutil"
+)
+
+{{ range .Tests -}}
+func Test_{{.ID}}_{{.Type}}{{.Name}}{{.Outcome}}(t *testing.T) {
+	{{- if .Implemented}}
+	if os.Getenv("GO_RST_SKIP_NOT_IMPLEMENTED") == "1" {
+		t.SkipNow()
+	}
+	{{- end }}
+	testPath := testutil.TestPathFromName("{{.FileBaseName}}")
+	test := LoadParserTest(t, testPath)
+	{{- if eq .Type "Lexer"}}
+	items := lexTest(t, test)
+	equal(t, test.ExpectItems(), items)
+	{{- else}}
+	pTree := parseTest(t, test)
+	eNodes := test.ExpectNodes()
+	checkParseNodes(t, eNodes, pTree.Nodes, testPath)
+	{{- end}}
+}
+
+{{ end -}}
+`
+
+func capitalize(s string) (o string) {
+	n := strings.Split(s, "-")
+	for _, v := range n {
+		o = o + strings.Title(v)
+	}
+	return
+}
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return err
+	}
+	fp := strings.Split(path, string(os.PathSeparator))
+	re := regexp.MustCompile(`(([\d\.]{4,})-(bad-)?(.*))(-items|-nodes)(-xx)?\.json`)
+	js := re.FindStringSubmatch(path)
+	if js != nil {
+		// Example object:
+		// []string{"11.00.00.00-three-short-options-nodes.json", "11.00.00.00-three-short-options", "11.00.00.00", "", "three-short-options", "-nodes", "", "json"}
+		re2 := regexp.MustCompile(`[\d]*-test-(.*)`)
+		m := testFile{name: re2.FindStringSubmatch(fp[1])[1], baseName: js[1], id: js[2], path: path}
+		if js[3] != "" {
+			m.bad = true
+		}
+		if js[5] == "-items" {
+			m.lexerTest = true
+		} else if js[5] == "-nodes" {
+			m.parserTest = true
+		}
+		if js[6] == "" {
+			m.implemented = true
+		}
+		testFiles = append(testFiles, m)
+	}
+	return err
+}
+
+func main() {
+	testFiles = make([]testFile, 0, 100)
+	err := filepath.Walk("testdata", walkFunc)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	tokenTestSuite := testPackage{
+		Name:       "token",
+		Tests:      make([]test, 0, 100),
+		OutputPath: "pkg/token/rst_test.go",
+	}
+	parserTestSuite := testPackage{
+		Name:       "parser",
+		Tests:      make([]test, 0, 100),
+		OutputPath: "pkg/parser/rst_test.go",
+	}
+
+	for _, v := range testFiles {
+		t := "Lexer"
+		p := "parser"
+		if v.parserTest {
+			t = "Parser"
+			p = "token"
+		}
+
+		o := "Good"
+		if v.bad {
+			o = "Bad"
+		}
+
+		id := strings.Replace(v.id, ".", "_", -1)
+		m := test{
+			Package:      p,
+			ID:           id,
+			Type:         t,
+			Name:         capitalize(v.name),
+			FileBaseName: v.baseName,
+			Outcome:      o,
+			Implemented:  v.implemented,
+		}
+
+		if v.lexerTest {
+			tokenTestSuite.Tests = append(tokenTestSuite.Tests, m)
+		} else {
+			parserTestSuite.Tests = append(parserTestSuite.Tests, m)
+		}
+	}
+
+	tmp := template.Must(template.New("tests").Parse(testTemplate))
+	var testObj []testPackage
+	testObj = append(testObj, tokenTestSuite)
+	testObj = append(testObj, parserTestSuite)
+
+	for _, v := range testObj {
+		f, err := os.Create(v.OutputPath)
+		if err != nil {
+			fmt.Println("\n\nERROR Create file: ", err)
+			return
+		}
+		err = tmp.Execute(f, v)
+		if err != nil {
+			fmt.Printf("\n\nERROR executing template: %s", err)
+			os.Exit(1)
+		}
+		f.Close()
+	}
+
+}
